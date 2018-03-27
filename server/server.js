c658a6d2a7f1fa09094df924bcecc695a0b505a9
ilmam <ilmam.mourtada@gmail.com>
2018-03-27 16:31:26 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
+var WebSocketServer = require('requireg')('ws').Server,
+wss = new WebSocketServer({port: 8181});
 
+wss.broadcast = function(data) {
+    for (var i in this.clients)
+        this.clients[i].send(data);
+};
+
+wss.on('connection', function(ws) {
+    console.log('client connected');
+    ws.on('message', function(message) {
+        wss.broadcast(message);
+        console.log(message);
+    });
+});
